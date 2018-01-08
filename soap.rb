fad36f7dda35ba18d96633369224efdf40ff166b
GitGod <barmazu23@gmail.com>
2018-01-08 15:12:34 +0100

    Update soap.rb

--- soap.rb
+++ soap.rb
@@ -1,1 +1,14 @@
-so
+class Icecream
+  attr_accessor :id, :taste, :type, :price
+
+  def initialize(id, taste, type, price)
+    @id = id
+    @taste = taste
+    @model = type
+    @price = price
+  end
+
+  def to_s
+    "#{taste} #{type} #{price}"
+  end
+end
