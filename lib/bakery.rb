3ff109afe50d8e59ba154165013302e99d46b82a
mwieckowsk <32486835+mwieckowsk@users.noreply.github.com>
2018-01-08 21:50:52 +0100

    Update bakery.rb

--- lib/bakery.rb
+++ lib/bakery.rb
@@ -1,49 +1,61 @@
 class Bakery
   attr_accessor :someone, :icecream
- @@someone = []
- @@icecream = []
+  @@someone = []
+  @@icecream = []
 
   
  #osoba
- def show_someone(id)
-   if !@@someone.at(id).nil?
-    puts "Person with id = #{id}: #{@@someone[id]}"
-    return @@someone[id]
-   else
-    puts "Person with id = #{id} never existed. Please add new person to the database"
-    return false
-   end
- end  
- def edit_someone(id, firstname, lastname)
-   if !@@people.at(id).nil?
-    insert_data_edit_person(id, firstname, lastname, country, age)
-    puts "Person with id = #{@@people[id].id} has been updated."
-   else
-    puts "Person with id = #{id} doesn't appear in database."
-   end
- end  
- def delete_someone(id)
- end  
+  def show_someone(id)
+    if !@@someone.at(id).nil?
+      puts "Person with id = #{id}: #{@@someone[id]}"
+      return @@someone[id]
+    else
+      puts "Person with id = #{id} never existed. Please add new person to the database"
+      return false
+    end
+  end
+  
+  def add_someone(id, firstname, lastname)
+    
+  end
+ 
+  def edit_someone(id, firstname, lastname)
+    if !@@someone.at(id).nil?
+      insert_data_edit_person(id, firstname, lastname)
+      puts "Person with id = #{@@someone[id].id} has been updated."
+    else
+      puts "Person with id = #{id} doesn't appear in database."
+    end
+  end  
+ 
+  def delete_someone(id)
+   
+  end  
  
  #lody
- def show_ice(id)
-  if !@@icecream.at(id).nil?
-   puts "Icecream with id = #{id}:
-    taste = #{@@icecream[id].taste} ,
-    model = #{@@icecream[id].model} ,
-    price = #{@@icecream[id].price}."
-  else
-    puts "Icecream with id = #{id} doesn't appear to exist. Please add a new one."
+  def show_ice(id)
+    if !@@icecream.at(id).nil?
+      puts "Icecream with id = #{id}:
+      taste = #{@@icecream[id].taste} ,
+      model = #{@@icecream[id].model} ,
+      price = #{@@icecream[id].price}."
+    else
+      puts "Icecream with id = #{id} doesn't appear to exist. Please add a new one."
+    end
   end
- end
  
- def add_ice
- end  
- def edit_ice
- end  
- def delete_ice(id)
- end
+  def add_ice
  
- #cos
+  end  
+  
+  def edit_ice
+  
+  end  
+ 
+  def delete_ice(id)
+  
+  end
+ 
+  #cos
 
 end
