52f6c7d67561dd9976e4709a693f8d0ebdd87ca0
mwieckowsk <32486835+mwieckowsk@users.noreply.github.com>
2018-01-08 21:23:47 +0100

    Update bakery.rb

--- lib/bakery.rb
+++ lib/bakery.rb
@@ -1,5 +1,17 @@
 Class Bakery
   attr_accessor :someone, :icecream
-@@someone = []
-@@icecream = []
+ @@someone = []
+ @@icecream = []
+
+ def show_ice(id)
+  if !@@icecream.at(id).nil?
+    puts "Icecream with id = #{id}:
+     taste = #{@@books[id].taste} ,
+     model = #{@@books[id].model} ,
+     price = #{@@books[id].price}."
+    puts "Icecream with id = #{id} doesn't appear in database."
+  end
+ end
+
+
 end
