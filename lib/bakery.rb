bcca6eed48249dc2880362fb15945f85cc81a617
GitGod <barmazu23@gmail.com>
2018-01-08 22:30:04 +0100

    Update bakery.rb

--- lib/bakery.rb
+++ lib/bakery.rb
@@ -53,7 +53,7 @@ class Bakery
     if !@@icecream.at(id).nil?
       puts "Icecream with id = #{id}:
       taste = #{@@icecream[id].taste} ,
-      model = #{@@icecream[id].model} ,
+      type = #{@@icecream[id].type} ,
       price = #{@@icecream[id].price}."
     else
       puts "Icecream with id = #{id} doesn't appear to exist. Please add a new one."
@@ -69,14 +69,14 @@ class Bakery
     puts "Added new ice cream to database:
     id - #{@@icecream[id].id},
     taste - #{@@icecream[id].taste},
-    model - #{@@icecream[id].model},
+    type - #{@@icecream[id].type},
     price - #{@@icecream[id].price}."
   end  
   
   def edit_ice(id, taste, model, price)
     if !@@icecream.at(id).nil?
       @@icecream[id].taste = taste if taste != ''
-      @@icecream[id].model = model if model != ''
+      @@icecream[id].type = type if type != ''
       @@icecream[id].price = price if price != ''
       puts "Ice cream with id = #{@@icecream[id].id} has been updated."
     else
