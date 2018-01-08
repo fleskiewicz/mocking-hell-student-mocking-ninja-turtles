bc2fba8bec1d13075791ebb2b822235dc3b3a236
mwieckowsk <32486835+mwieckowsk@users.noreply.github.com>
2018-01-08 21:46:32 +0100

    Update bakery.rb

--- lib/bakery.rb
+++ lib/bakery.rb
@@ -1,17 +1,49 @@
-Class Bakery
+class Bakery
   attr_accessor :someone, :icecream
  @@someone = []
  @@icecream = []
 
+  
+ #osoba
+ def show_someone(id)
+   if !@@someone.at(id).nil?
+    puts "Person with id = #{id}: #{@@someone[id]}"
+    return @@someone[id]
+   else
+    puts "Person with id = #{id} never existed. Please add new person to the database"
+    return false
+   end
+ end  
+ def edit_someone(id, firstname, lastname)
+   if !@@people.at(id).nil?
+    insert_data_edit_person(id, firstname, lastname, country, age)
+    puts "Person with id = #{@@people[id].id} has been updated."
+   else
+    puts "Person with id = #{id} doesn't appear in database."
+   end
+ end  
+ def delete_someone(id)
+ end  
+ 
+ #lody
  def show_ice(id)
   if !@@icecream.at(id).nil?
-    puts "Icecream with id = #{id}:
-     taste = #{@@books[id].taste} ,
-     model = #{@@books[id].model} ,
-     price = #{@@books[id].price}."
-    puts "Icecream with id = #{id} doesn't appear in database."
+   puts "Icecream with id = #{id}:
+    taste = #{@@icecream[id].taste} ,
+    model = #{@@icecream[id].model} ,
+    price = #{@@icecream[id].price}."
+  else
+    puts "Icecream with id = #{id} doesn't appear to exist. Please add a new one."
   end
  end
-
+ 
+ def add_ice
+ end  
+ def edit_ice
+ end  
+ def delete_ice(id)
+ end
+ 
+ #cos
 
 end
