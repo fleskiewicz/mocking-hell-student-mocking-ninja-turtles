d44c890a24ca246ec031e5682e8568d4c0620316
mwieckowsk <32486835+mwieckowsk@users.noreply.github.com>
2018-01-08 21:57:21 +0100

    Update bakery.rb

--- lib/bakery.rb
+++ lib/bakery.rb
@@ -21,7 +21,8 @@ class Bakery
  
   def edit_someone(id, firstname, lastname)
     if !@@someone.at(id).nil?
-      insert_data_edit_person(id, firstname, lastname)
+      @@someone[id].firstname = firstname if firstname != ''
+      @@someone[id].lastname = lastname if lastname != ''
       puts "Person with id = #{@@someone[id].id} has been updated."
     else
       puts "Person with id = #{id} doesn't appear in database."
@@ -29,7 +30,12 @@ class Bakery
   end  
  
   def delete_someone(id)
-   
+    if !@@someone.at(id).nil?
+      @@someone[id] = nil
+      puts "Person with id = #{id} has been removed."
+    else
+      puts "Person with id = #{id} doesn't seem to exist."
+    end
   end  
  
  #lody
