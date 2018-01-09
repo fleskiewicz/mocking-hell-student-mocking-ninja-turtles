7e0ee2301f8ac9a18adee767ebd057370bc0fac5
mwieckowsk <32486835+mwieckowsk@users.noreply.github.com>
2018-01-09 01:00:13 +0100

    Update icecream.rb

--- lib/icecream.rb
+++ lib/icecream.rb
@@ -1,14 +1,38 @@
 class Icecream
-  attr_accessor :id, :taste, :type, :price
+  attr_accessor :id, :flavor, :type, :price
 
-  def initialize(id, taste, type, price)
+  def initialize(id, flavor, type, price)
     @id = id
-    @taste = taste
+    @flavor = flavor
     @type = type
     @price = price
   end
 
   def to_s
-    "#{taste} #{type} #{price}"
+    "#{flavor} #{type} #{price}"
   end
+  
+  def validate
+    raise ArgumentError, 'Invalid ID' unless valid_id?
+    raise ArgumentError, 'Invalid flavor' unless valid_flavor?
+    raise ArgumentError, 'Invalid type' unless valid_type?
+    raise ArgumentError, 'Invalid price' unless valid_price?
+  end
+
+  def valid_id?
+    @id.is_a?(Numeric)
+  end
+
+  def valid_flavor?
+    @flavor.is_a?(String) && @flavor.length >= 1 && @flavor.length <= 20 && @flavor.chars.first =~ /[A-Z]/
+  end
+
+  def valid_type?
+    @type.is_a?(String) && @type.length >= 1 && @type.length <= 20 && @type.chars.first =~ /[A-Z]/
+  end
+
+  def valid_price?
+    @price.is_a?(Numeric)
+  end
+  
 end
