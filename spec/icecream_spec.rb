87b9dd6dca91894c363dcea18234151f7936a59a
azbrowski <32486117+azbrowski@users.noreply.github.com>
2018-01-09 02:23:31 +0100

    Update icecream_spec.rb

--- spec/icecream_spec.rb
+++ spec/icecream_spec.rb
@@ -37,7 +37,7 @@ RSpec.describe 'Icecream' do
   end
 
   context '#to_s' do
-    subject(:icecream) { Icecream.new 1, 'Chocolate', 'horn', '50' }
+    subject(:icecream) { Icecream.new 1, 'Chocolate', 'horn', 50 }
 
     it 'returns correct output' do
       expect(icecream.to_s).to be_a(String).and include('Chocolate horn 50')
