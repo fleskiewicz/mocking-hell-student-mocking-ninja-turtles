cfaf3692f671f2f3f0a340e60157925174b6ef14
azbrowski <32486117+azbrowski@users.noreply.github.com>
2018-01-09 14:34:18 +0100

    Add files via upload

--- spec/bill_spec.rb
+++ spec/bill_spec.rb
@@ -1,7 +1,7 @@
 require_relative '../lib/bill.rb'
 
 RSpec.describe 'Bill' do
-  context '#initializew' do
+  context '#initialize' do
     let(:id) { 1 }
     let(:name) { 'John' }
     let(:products) { 'cookie' }
@@ -15,13 +15,13 @@ RSpec.describe 'Bill' do
     end
 
     describe 'when return correct bill id' do
-      it {      
+      it {
 	expect(bill.id).to eq(id)
       }
     end
 
     describe 'when return correct customer name' do
-      it {      
+      it {
 	expect(bill.name).to eq(name)
       }
     end
@@ -35,6 +35,34 @@ RSpec.describe 'Bill' do
     describe 'when return correct bill cost' do
       it {
         expect(bill.cost).to eq(cost)
+      }
+    end
+  end
+
+  context '#validate' do
+    subject(:bill) { Bill.new 1, 'John', 'cookie', 22 }
+
+    describe 'when validate id' do
+      it {
+        expect(bill.valid_id?).to eq(true)
+      }
+    end
+
+    describe 'when validate name' do
+      it {
+        expect(bill.valid_name?).to eq(true)
+      }
+    end
+
+    describe 'when validate products' do
+      it {
+        expect(bill.valid_products?).to eq(true)
+      }
+    end
+
+    describe 'when validate cost' do
+      it {
+        expect(bill.valid_cost?).to eq(true)
       }
     end
   end
