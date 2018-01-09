4b11dddf86acc483643664fe2c97f8bb0e9c0fab
azbrowski <32486117+azbrowski@users.noreply.github.com>
2018-01-09 08:07:48 +0100

    Update flavor_controller.rb

--- lib/flavor_controller.rb
+++ lib/flavor_controller.rb
@@ -1,22 +1,27 @@
 class FlavorController
   attr_accessor :flavors
 
-	def initialize(flavors = [])
-		@flavors = flavors
-	end
-	
-	def get_flavor(id)
-		@flavors.find { |f| f.id == id }
+  def initialize(flavors = [])
+    @flavors = flavors
+  end
+
+  def get_flavor(id)
+    @flavors.find { |f| f.id == id }
+  end
+
+	def get_last_id
+		@flavors.last.nil? ? 0 : @flavors.last.id
 	end
 	
   def add_flavor(name, price)
-		id = @flavors.last.nil? ? 1 : @flavors.last.id+1
+		id = get_last_id + 1
     new_flavor = Flavor.new(id, name, price)
     @flavors.push(new_flavor)
   end
 	
-  def update_flavor(id, modified_flavor)
+  def update_flavor(id, new_name, new_price)
 		flavor = get_flavor(id)
+		modified_flavor = Flavor.new(id, new_name, new_price)
 		copy_flavor(flavor, modified_flavor)
   end  	
 	
@@ -27,24 +32,8 @@ class FlavorController
 		current_flavor
 	end
 	
-  def delete_flavor(id) 
-		@flavors.delete_if{ |flavor| flavor.id == id }
-  end
-	
-  def show_flavor(id)
-    if !@@icecream.at(id).nil?
-      puts "Icecream with id = #{id}:
-      taste = #{@@icecream[id].taste} ,
-      type = #{@@icecream[id].type} ,
-      price = #{@@icecream[id].price}."
-    else
-      puts "Icecream with id = #{id} doesn't appear to exist. Please add a new one."
-    end
-  end
+	def remove_flavor(id)
+		@flavors.delete_if { |f| f.id == id }
+	end
 
-  
-  def insert_data_edit_flavor(id, name, price)
-    @@flavor[id].name = name if name != ''
-    @@flavor[id].price = price if price != ''
-  end
 end
