8fe56cec40f8a6c697c02110d6e29c880909a1c1
GitGod <barmazu23@gmail.com>
2018-01-09 12:01:26 +0100

    Update icecream_controller_spec.rb

--- spec/icecream_controller_spec.rb
+++ spec/icecream_controller_spec.rb
@@ -12,48 +12,7 @@ RSpec.describe IcecreamController do
       end
     end
 
-    describe '.add_icecream' do
-      let(:icecream) { icecreamFactory.create_single_icecream }
-      let(:expected_number) { 1 }
-
-      it 'adds new icecream to the storage' do
-        manager.add_icecream(icecream)
-        expect(manager.icecream_count).to eq(expected_number)
-        expect(manager.get_icecream(icecream.id)).to eq(icecream)
-      end
-    end
-  end
-
-  context 'with some icecreams' do
-    subject(:manager) { IcecreamController.new icecreams }
-
-    describe '.icecream_count' do
-      let(:expected_number) { icecreams.count }
-
-      it 'returns correct number of icecreams' do
-        expect(manager.icecream_count).to eq(expected_number)
-      end
-    end
-
-    describe '.remove_icecream' do
-      let!(:expected_number) { manager.icecream_count - 1 }
-      let(:id) { 1 }
-
-      it 'removes certain icecream' do
-        icecream = manager.remove_icecream(id)
-        expect(manager.icecream_count).to eq(expected_number)
-        expect(manager.icecreams).not_to include(icecream)
-      end
-    end
-
-    describe '.update_icecream' do
-
-      it 'updates certain icecream' do
-        expect(manager).to receive(:get_icecream).and_return(icecreams[1], icecreams[1])
-        manager.update_icecream(id, updated_icecream)
-        expect(manager.get_icecream(id).title).to eq(updated_icecream.title)
-      end
-    end
+    
 
     describe '.get_icecream' do
       let(:target) { icecreams[1] }
