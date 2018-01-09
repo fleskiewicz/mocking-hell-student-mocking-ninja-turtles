1ebc25abdbaeb1c01e42a3800ab05b097dbfdb46
GitGod <barmazu23@gmail.com>
2018-01-09 11:37:54 +0100

    Update icecream_controller_spec.rb

--- spec/icecream_controller_spec.rb
+++ spec/icecream_controller_spec.rb
@@ -4,16 +4,7 @@ RSpec.describe icecream_controller do
   context 'without icecreams' do
     subject(:controller) { icecream_controller.new }
 
-    describe '.icecream_count' do
-      let(:expected_number) { 0 }
-
-      it 'returns 0' do
-        expect(controller.icecream_count).to eq(expected_number)
-      end
-end
-
 describe '.remove_icecream' do
-      let!(:expected_number) { controller.icecream_count - 1 }
       let(:id) { 1 }
 
       it 'removes certain icecream' do
